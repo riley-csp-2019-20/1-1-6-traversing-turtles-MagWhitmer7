557bd14a914d8af3b3922efbe84ea6b83f5b471b
MagWhitmer7 <55501077+MagWhitmer7@users.noreply.github.com>
2019-09-20 11:36:06 -0400

    2

--- 116MargaretWhitmer.py
+++ 116MargaretWhitmer.py
@@ -7,27 +7,35 @@ my_turtles = []
 
 # use interesting shapes and colors
 turtle_shapes = ["arrow", "turtle", "circle", "square", "triangle", "classic"]
-turtle_colors = ["red", "blue", "green", "orange", "purple", "gold"]
+turtle_colors = ["red", "orange", "yellow", "green", "blue", "purple"]
 
 for s in turtle_shapes:
   t = trtl.Turtle(shape=s)
   my_turtles.append(t)
 
-#  set starting place
+#  set starting place and direction
 startx = 0
 starty = 0
+direction=90
 
 # move turtles so they don't overlap
 for t in my_turtles:
+    new_color=turtle_colors.pop()
+    t.pencolor(new_color)
+    t.fillcolor(new_color)
+    t.begin_fill()
+    t.setheading
     t.penup()
+    t.end_fill()
     t.goto(startx, starty)
     t.right(45)     
     t.forward(50)
     t.pendown()
+    t.end_fill()
 
-#	
-    startx = startx + 50
-    starty = starty + 50
+#	set new start x and start y
+    startx = t.xcor()
+    starty = t.ycor()
 
 wn = trtl.Screen()
 wn.mainloop()
